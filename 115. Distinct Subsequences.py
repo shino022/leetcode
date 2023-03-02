21c2cd0e1c646cf65fdc3ef993c1d8add865a621
shin <sshoony95@gmail.com>
2023-03-02 11:04:32 -0500

    Add a solution

--- 115. Distinct Subsequences.py
+++ 115. Distinct Subsequences.py
@@ -18,3 +18,22 @@ The base cases are:
 
 The function dfs is a recursive function that takes two indices i and j as input and returns the number of distinct subsequences of s[i:] that match t[j:]. The function numDistinct is the entry point of the algorithm and returns the result of calling dfs with i=0 and j=0. The memoization table dp is used to cache the results of the subproblems to avoid redundant computation.
 """
+
+class Solution:
+  def numDistinct(self, s: str, t: str) -> int:
+    dp = {}
+    def dfs(i, j):
+      if j == len(t):
+        return 1
+      elif i == len(s):
+        return 0
+      elif (i, j) in dp:
+        return dp[(i, j)]
+      
+      if s[i] == t[j]:
+        dp[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+        return dp[(i, j)]
+      else:
+        dp[(i, j)] = dfs(i + 1, j)
+        return dp[(i, j)]
+    return dfs(0, 0)
